6d15b1d1c6949580e78f3af9bf6f72fb86de1a7c
blndev <blndev@users.noreply.github.com>
2025-01-05 19:48:28 +0100

    introduce xformers and more debug output

--- photo-to-drawing.py
+++ photo-to-drawing.py
@@ -5,12 +5,13 @@ import os
 from diffusers import StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
 from PIL import Image, ImageDraw
 from transformers import pipeline # for captioning
+from xformers.ops import MemoryEfficientAttentionFlashAttentionOp
 
 DEBUG = True
 
 # TODO: load from config
 #modelname = "./models/SD1.5/SFW/wildcardxRealistic_wildcardxV2.safetensors
-modelname = "./models/wwtoonMix_v10.safetensors"
+CURRENT_IMAGE_2_IMAGE_MODEL_NAME = "./models/wwtoonMix_v10.safetensors"
 
 # "runwayml/stable-diffusion-inpainting"
 # "CompVis/stable-diffusion-v1-4"
@@ -32,6 +33,10 @@ def get_all_local_models():
     except Exception as e:
         print(e)
     return safetensors_files
+
+def update_all_local_models():
+    """updates the list of available models in the ui"""
+    return gr.update(choices=get_all_local_models())
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print (f"running on {device}")
@@ -103,37 +108,51 @@ def describe_image(image):
     return value[0]['generated_text']
 
 IMAGE_TO_IMAGE_PIPELINE = None
-def load_model(model=modelname):
+def load_model(model=CURRENT_IMAGE_2_IMAGE_MODEL_NAME):
     """Load and return the Stable Diffusion model based on style."""
     global IMAGE_TO_IMAGE_PIPELINE
     if (IMAGE_TO_IMAGE_PIPELINE!= None): 
         if DEBUG: print ("using cached model")
         return IMAGE_TO_IMAGE_PIPELINE
 
-    #TODO V3: support SDXL or FLux
-    #pipeline = StableDiffusionPipeline.from_pretrained(
-    #pipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
-    if DEBUG: print(f"create pipeline for model {model}")
-    pipeline = StableDiffusionImg2ImgPipeline.from_single_file(
-        model,
-        torch_dtype=torch.float16 if device == "cuda" else torch.float32,
-        safety_checker = None, requires_safety_checker = False
-        #revision="fp16" if device == "cuda" else "",
-    )
-    pipeline = pipeline.to(device)
-    return pipeline
+    try:
+        #TODO V3: support SDXL or FLux
+        #pipeline = StableDiffusionPipeline.from_pretrained(
+        #pipeline = StableDiffusionImg2ImgPipeline.from_pretrained(
+        if DEBUG: print(f"create pipeline for model {model}")
+        pipeline = StableDiffusionImg2ImgPipeline.from_single_file(
+            model,
+            torch_dtype=torch.float16 if device == "cuda" else torch.float32,
+            safety_checker = None, requires_safety_checker = False
+            #use_safetensors=True
+            #revision="fp16" if device == "cuda" else "",
+        )
+        if DEBUG: print(f"- pipeline initiated")
+        pipeline = pipeline.to(device)
+        pipeline.enable_xformers_memory_efficient_attention()
+        if DEBUG: print("-->pipeline created")
+        return pipeline
+    except Exception as e:
+        print(f"pipeline not created. Error in load_model: {e}")
+        if not gr is None:
+            gr.Warning(message="Error while loading the model. See logfile for details.")
+
 
 def debug_reload_model(model):
     if not DEBUG: return
     global IMAGE_TO_IMAGE_PIPELINE
+    global CURRENT_IMAGE_2_IMAGE_MODEL_NAME
     print (f"Reload model {model}")
     try:
         IMAGE_TO_IMAGE_PIPELINE = None
         if device=="cuda": torch.cuda.empty_cache()
         IMAGE_TO_IMAGE_PIPELINE = load_model(model)
-        gr.Info(message=f"Model {model} loaded.")
+        if IMAGE_TO_IMAGE_PIPELINE is None: raise Exception("Pipeline is none")
+        gr.Info(message=f"Model {model} loaded.", title="Model changed")
+        CURRENT_IMAGE_2_IMAGE_MODEL_NAME = model
     except Exception as e:
-        gr.Error(message=f"Loading Model {model} failed. \n\n {e}")
+        print(f"model not loaded. {e}")
+        gr.Warning(message=f"Loading Model {model} failed. \n\n {e}")
 
 def stylize(image, style, strength, steps, image_description):
     """Convert the entire input image to the selected style."""
@@ -158,11 +177,11 @@ def stylize(image, style, strength, steps, image_description):
             negative_prompt = "realistic photo"
             strength = 0.5
         elif style == "Manga":
-            prompt = "manga style"
+            prompt = "manga style, perfect drawing,smiling"
             negative_prompt = "realistic photo"
             strength = 0.5
         elif style == "Painting":
-            prompt = "modern oil painting of"
+            prompt = "modern oil painting of smiling"
             strength = 0.5
             negative_prompt = "realistic photo"
         elif style == "Fantasy":
@@ -180,6 +199,7 @@ def stylize(image, style, strength, steps, image_description):
         else:
             # just use description for creativity purpose 
             prompt = ""
+            negative_prompt = ""
         
         prompt += f": {image_description}"
 
@@ -200,7 +220,7 @@ def stylize(image, style, strength, steps, image_description):
             prompt=prompt, 
             negative_prompt=negative_prompt,
             num_inference_steps=steps, 
-            image=image, 
+            image=image,
             mask_image=mask, 
             strength=strength,
             ).images[0]
@@ -223,19 +243,26 @@ with gr.Blocks() as app:
         gr.Markdown("*DEBUG enabled*")
         with gr.Row():
             with gr.Column():
-                model_dropdown = gr.Dropdown(choices=get_all_local_models(), value=modelname, label="Models")
+                model_dropdown = gr.Dropdown(choices=get_all_local_models(), value=CURRENT_IMAGE_2_IMAGE_MODEL_NAME, label="Models")
             with gr.Column():
+                refresh_model_list_button = gr.Button("reload model list")
                 reload_button = gr.Button("reload model")
                 reload_button.click(
                     fn=debug_reload_model,
                     inputs=[model_dropdown],
                     outputs=[]
                 )
+                refresh_model_list_button.click(
+                    fn=update_all_local_models,
+                    inputs=[],
+                    outputs=[model_dropdown]
+                )
     with gr.Row():
         with gr.Column():
             image_input = gr.Image(label="Input Image", type="pil", height=512)
             describe_button = gr.Button("Describe", interactive=False)
             text_description = gr.Textbox(label="Image description for better results")
+        with gr.Column():
             #TODO: load from config
             styles = [
                 "Anime",
@@ -250,9 +277,9 @@ with gr.Blocks() as app:
             style_dropdown = gr.Radio(styles, label="Style", value="Anime")
             strength_slider = gr.Slider(label="Strength", minimum=0.1, maximum=1, value=0.4, step=0.1)
             steps_slider = gr.Slider(label="Steps", minimum=10, maximum=100, value=75, step=5)
-        with gr.Column():
+
             output_image = gr.Image(label="Output Image", type="pil", height=512)
-            start_button = gr.Button("Start", interactive=False)
+            start_button = gr.Button("Create", interactive=False)
 
     # Save input image immediately on change
     image_input.change(
