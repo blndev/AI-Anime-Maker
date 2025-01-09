20d8ffd8d02957ce0a271e416f6e07c01abf93f8
blndev <blndev@users.noreply.github.com>
2025-01-09 11:58:51 +0100

    add startup script

--- run.sh
+++ run.sh
@@ -1,4 +1,33 @@
-#check venv
-#install if not existing
-#start app 
-#TODO support Parameters if required
+#!/bin/bash
+
+# Check if ".venv" folder exists, if not create a virtual environment
+if [ ! -d ".venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv .venv
+else
+    echo "Virtual environment already exists."
+fi
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# # Check if "models/toony.safetensors" exists, if not download it
+# if [ ! -f "models/toonify.safetensors ]; then
+#     echo "Downloading toony.safetensors..."
+#     mkdir -p models
+#     wget -O models/toonify.safetensors "https://civitai.com/api/download/models/244831?type=Model&format=SafeTensor&size=pruned&fp=fp16"
+# else
+#     echo "Model file already exists."
+# fi
+
+# Upgrade Python requirements
+echo "Upgrading Python requirements..."
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Execute "photo-to-drawing.py" in the virtual environment
+echo "Starting app..."
+python photo-to-drawing.py
+
+# Deactivate the virtual environment
+deactivate
