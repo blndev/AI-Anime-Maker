b3540638d6ff6666f167e9bf22e0212c8a221532
blndev <blndev@users.noreply.github.com>
2025-01-23 16:11:36 +0100

    introduce analytics support

---
+++ analytics.py
@@ -0,0 +1,11 @@
+# Features provided by this file
+# * statistics about used styles and assoziated prompts
+# * if store output and analytics are activated, prompts used for the generated output are stored
+# * if cache, store output and analytics are activated, input and resulting images are connected 
+
+import gradio as gr
+import sqlite3
+
+
+
+
