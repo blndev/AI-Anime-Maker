c15aacde4341b8934c9df83d40f7f48012ad4074
blndev <blndev@users.noreply.github.com>
2025-03-04 17:42:07 +0100

    add params in base class

--- src/genai/BaseGenAIHandler.py
+++ src/genai/BaseGenAIHandler.py
@@ -1,17 +1,17 @@
 from abc import ABC, abstractmethod
 from typing import List
-from PIL import Image
+from PIL.Image import Image
 
 from .ImageGenerationParameters import GenerationParameters
 
 
 class BaseGenAIHandler(ABC):
     @abstractmethod
-    def generate_images(self, params: GenerationParameters, count: int) -> List[Image.Image]:
+    def generate_images(self, params: GenerationParameters, count: int) -> List[Image]:
         """Generate a list of images."""
         pass
 
-    def generate_image(self, params: GenerationParameters) -> Image.Image:
+    def generate_image(self, params: GenerationParameters) -> Image:
         """Generate a single image by calling generate_images with count 1."""
         images = self.generate_images(params=params, count=1)
         return images[0] if images else None
